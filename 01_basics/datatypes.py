1c83fbb30ac595d5e3191506f49eaf2201484218
itznagender <kumarnagender811@gmail.com>
2024-08-14 10:24:56 +0530

    Datatypes in python

---
+++ 01_basics/datatypes.py
@@ -0,0 +1,22 @@
+# Object types/ Data Types 
+
+"""
+#Datan Types
+1) Number -> 1234, 3.14, ob11, Decimal(), Fraction() 
+2) String -> 'spam' "Nagender", b'a/x01c'
+3) List -> [1,2,3], [1,[2,"three"],4,5],list(range(10))
+4) Tuple -> (1,'spam',4,'u'), tuple('spam), namedtuple
+5) Dictionary -> {'a':1,'b':2}, dic(zip(['a','b,],[1,2]))
+
+#Set -> set('abc'), {'a','b','c'}
+
+#File -> open('spam.txt', 'w')
+
+#Boolean -> True, False
+
+#None -> None
+
+#Function, module, classes
+
+#Advance Types -> Decorators, Generators, Iterators, metaProgramming
+"""
