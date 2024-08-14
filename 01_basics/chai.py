d67b1ff6061f5f4c3cf75dbface6b123fcdf2da3
itznagender <kumarnagender811@gmail.com>
2024-08-14 09:39:09 +0530

    Add first program

---
+++ 01_basics/chai.py
@@ -0,0 +1,3 @@
+from hello_chai import ichai
+
+chai("cold coffee")
---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,10 @@
+print("Hello Python")
+
+def chai(n):
+    print(n)
+
+chai("coffee")
+
+chai_one = "mint chai"
+chai_two = "ginger chai"
+chai_three = "masala_chai"
