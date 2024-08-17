5481f8af488af4861de80b9ab5ea56d7120a1740
itznagender <kumarnagender811@gmail.com>
2024-08-17 12:19:46 +0530

    add basics datatypes

--- 01_basics/list.py
+++ 01_basics/list.py
@@ -36,6 +36,6 @@ count = range(10)
 print(count)    #output: range(0, 10)  
 
 #square the no. from 0 to 10
+
 squared_num = [x**2 for x in range(10)]  
 print(squared_num)    #output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
-
